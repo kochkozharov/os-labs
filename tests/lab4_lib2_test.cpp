98bdaac726d4a796656d8bedc6ae39483b4bc741
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2023-12-17 16:43:53 +0300

    Unique ptr wrapper

--- tests/lab4_lib2_test.cpp
+++ tests/lab4_lib2_test.cpp
@@ -12,15 +12,13 @@ TEST(FourthLabTests, GCDTest) {
 }
 
 TEST(FourthLabTests, TranslationTest) {
-    auto *str = Translation(0);
-    EXPECT_TRUE(std::strcmp(str, "0") == 0);
-    std::free(str);
-    str = Translation(5);
-    EXPECT_TRUE(std::strcmp(str, "12") == 0);
-    std::free(str);
-    str = Translation(888);
-    EXPECT_TRUE(std::strcmp(str, "1012220") == 0);
-    str = Translation(-1);
-    EXPECT_EQ(str, nullptr);
-    std::free(str);
+    auto deleter = [](char *str) { std::free(str); };
+    std::unique_ptr<char, decltype(deleter)> str(Translation(0), deleter);
+    EXPECT_TRUE(std::strcmp(str.get(), "0") == 0);
+    str.reset(Translation(5));
+    EXPECT_TRUE(std::strcmp(str.get(), "12") == 0);
+    str.reset(Translation(888));
+    EXPECT_TRUE(std::strcmp(str.get(), "1012220") == 0);
+    str.reset(Translation(-1));
+    EXPECT_EQ(str.get(), nullptr);
 }
